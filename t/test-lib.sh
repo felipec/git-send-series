e808ea579116fe6fd43b56040c52f4b5a903fb5c
Felipe Contreras <felipe.contreras@gmail.com>
2023-04-01 04:52:49 -0600

    test: update to sharness 1.2
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -8,3 +8,28 @@ GIT_COMMITTER_EMAIL=committer@example.com
 GIT_COMMITTER_NAME='C O Mitter'
 export GIT_AUTHOR_EMAIL GIT_AUTHOR_NAME
 export GIT_COMMITTER_EMAIL GIT_COMMITTER_NAME
+
+test_cmp() {
+	${TEST_CMP:-diff -u} "$@"
+}
+
+test_when_finished() {
+	test_cleanup="{ $*
+		} && (exit \"\$eval_ret\"); eval_ret=\$?; $test_cleanup"
+}
+
+test_must_fail() {
+	"$@"
+	exit_code=$?
+	if test $exit_code = 0; then
+		echo >&2 "test_must_fail: command succeeded: $*"
+		return 1
+	elif test $exit_code -gt 129 -a $exit_code -le 192; then
+		echo >&2 "test_must_fail: died by signal: $*"
+		return 1
+	elif test $exit_code = 127; then
+		echo >&2 "test_must_fail: command not found: $*"
+		return 1
+	fi
+	return 0
+}
